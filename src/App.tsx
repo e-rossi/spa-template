d77edefa4332bb7dc175426a4a2b13a6c31e6dae
e-rossi <ericrossi1337@gmail.com>
2024-03-17 21:14:58 -0300

    Removed some defaults
    Signed-off-by: e-rossi <ericrossi1337@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
+    <div className="flex flex-col w-full h-full m-10 text-center">
+      <div className="flex flex-row justify-center m-10">
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="w-36 m-5" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="w-36 m-5" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className=" border-2">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,11 +24,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <p>Click on the Vite and React logos to learn more</p>
+    </div>
   );
 }
-
-export default App;
